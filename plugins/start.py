e7c87b0f8cc5cfc15271b2bb7ffd91b39cf7a6de
ialfa1 <81031778+ialfa1@users.noreply.github.com>
2021-07-14 16:06:47 +0000

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,9 +5,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("cantact me", url="https://www.instagram.com/lmx3")],
+        [InlineKeyboardButton("INSTAGRAM", url="https://www.instagram.com/lmx3")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/lmx33")]
+            "TELEGRAM", url="https://t.me/lmx33")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
