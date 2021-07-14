95ffb8eefbc86ddb329db51aaef1ce4c86506274
ialfa1 <81031778+ialfa1@users.noreply.github.com>
2021-07-14 19:47:04 +0400

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("cantact me", url="https://www.instagram.com/lmx3")],
+        [InlineKeyboardButton(
+            "Report Bugs 😊", url="https://t.me/lmx33")]
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
